e1b88f83f1fd07b9651549829772404c7b432cee
Arthur Wang <arthraim@gmail.com>
2017-02-20 16:35:15 +0800

    fix: bug fixes

--- ENIL/AppDelegate.swift
+++ ENIL/AppDelegate.swift
@@ -13,24 +13,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        if let u: URL = launchOptions?[UIApplicationLaunchOptionsKey.url] as? URL {
-            handleURL(url: u)
+
+        if let url: URL = launchOptions?[UIApplicationLaunchOptionsKey.url] as? URL {
+            updateUI(content: extractTextFromURL(url: url),
+                     shouldDelay: true)
+        } else {
+            updateUI(content: "NO AVAILABLE LINK\n\nClick link below to go back to MONSTER STRIKE, then send invitation link via LINE. It will automatically go back to this app.",
+                     shouldDelay: true)
         }
         return true
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        handleURL(url: url)
+        updateUI(content: extractTextFromURL(url: url),
+                 shouldDelay: false)
         return true
     }
 
-    func handleURL(url: URL) {
-//        let alert = UIAlertController(title: "1", message: url.absoluteString, preferredStyle: .alert)
-//        self.window?.rootViewController?.present(alert, animated: true, completion: nil)
-        (self.window?.rootViewController as! ViewController).textView.text
-            = url.path.replacingOccurrences(of: "/text/", with: "")
+    func extractTextFromURL(url: URL) -> String {
+        return url.path.replacingOccurrences(of: "/text/", with: "")
+    }
+
+    func updateUI(content: String, shouldDelay: Bool) {
+        let deadline = shouldDelay ? DispatchTime.now() + 1 : DispatchTime.now()
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            (self.window?.rootViewController as? ViewController)?.textView.text = content
+        }
     }
 
 }

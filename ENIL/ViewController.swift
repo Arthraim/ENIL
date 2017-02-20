68a447c10088bce44a643a053fc28c9c857da9a9
Arthur Wang <arthraim@gmail.com>
2017-02-20 15:18:12 +0800

    feat: finished most features already

--- ENIL/ViewController.swift
+++ ENIL/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func wechatAction(_ sender: Any) {
+        openUrlString(urlString: "weixin://")
+    }
 
+    @IBAction func telegramAction(_ sender: Any) {
+        openUrlString(urlString: "tg://")
+    }
+
+    @IBAction func monsterStrikingAction(_ sender: Any) {
+        openUrlString(urlString: "monsterstrike-app://")
+    }
+
+    func openUrlString(urlString: String) {
+        if let url = URL(string: urlString) {
+            if (UIApplication.shared.canOpenURL(url)) {
+                if #available(iOS 10, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "Cannot open this link", message: nil, preferredStyle: .alert)
+            UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
